a7dfbb18368d4f79a3b410c23d0cbb8b0a4f4bb3
Vidar Tonaas Fauske <vidartf@gmail.com>
2021-03-15 14:56:37 +0000

    Add "sync" mapping manager

---
+++ hotpot_km/mapping_sync.py
@@ -0,0 +1,14 @@
+
+from jupyter_server.services.kernels.kernelmanager import MappingKernelManager
+
+from .pooled_sync import SyncPooledKernelManager
+
+
+class SyncPooledMappingKernelManager(
+    SyncPooledKernelManager,
+    MappingKernelManager
+):
+    def restart_kernel(self, kernel_id, **kwargs):
+        if kwargs:
+            self.log.warning("Ignored arguments to restart_kernel: %r", kwargs)
+        return super().restart_kernel(kernel_id)
